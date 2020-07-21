41fbc9976376722b327514d671c39af6d0ac717d
HninWaiWaiHlaing <45447139+HninWaiWaiHlaing@users.noreply.github.com>
2020-07-21 15:13:27 +0630

    Refactoring multimodule maven project. Closes #1.

--- pet-clinic-web/src/main/java/com.springframework.petclinic/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com.springframework.petclinic/SpringPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringPetClinicApplication.class, args);
 	}
 
 }
